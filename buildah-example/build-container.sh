aff6543ac9b23959f72e9aaea8e3403762d4db08
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2019-10-07 14:05:20 +0200

    More work on the presentation.

---
+++ buildah-example/build-container.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# bc-as-a-service
+# No more Dockerfile!
+FEDORA_RELEASE=31
+FEDORA_IMAGE=registry.fedoraproject.org/fedora-minimal:${FEDORA_RELEASE}
+
+container=$(buildah from ${FEDORA_IMAGE})
+DNF_TOOL=microdnf
+
+buildah run $container -- $DNF_TOOL -y install nmap-ncat bc
+buildah run $container -- $DNF_TOOL clean all
+
+buildah run $container -- mkdir /logs
+buildah config --volume /logs $container
+
+buildah add $container entrypoint.sh /
+buildah config --entrypoint /entrypoint.sh $container
+
+buildah commit $container bcaas:latest
+
